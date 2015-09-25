3b07277068553f87c8c8e030282ffefe53e40b1d
RogerWoollett <roger@woollett.org.uk>
2015-09-25 09:49:04 +0100

    Update to show mode

--- PyMon.py
+++ PyMon.py
@@ -31,7 +31,7 @@ port = 8888
 
 REFRESH_RATE = 200  # msec between updates
 COLS = 5            # columns in display
-TITLE = 'Python GPIO Monitor'
+TITLE = 'Python GPIO Monitor - '
 
 # Pins to test (Broadcom,physical)
 # N.B. this list is for B+ & A+ earlier pis do not have so many pins
@@ -39,6 +39,9 @@ pins = ((2,3),(3,5),(4,7),(5,29),(6,31),(7,26),(8,24),
         (9,23),(10,19),(11,23),(12,32),(13,33),(14,8),
         (15,10),(16,36),(17,11),(18,12),(19,35),(20,38),
         (21,40),(22,15),(23,16),(24,18),(25,22),(26,37),(27,13))
+
+# character stings for modes
+modes = ('INPUT','OUTPUT','ALT5','ALT4','ALT0','ALT1','ALT2','ALT3')
 
 class Config(sd.Dialog):
     def __init__(self,master,msg,*args,**kwargs):
@@ -99,26 +102,37 @@ class PinFrame(tk.Frame):
         self.pin = pin
         self.mask = 1<<pin
         self.high = False
+        self.mode = -1
         
         # create a label (shows GPIO with physical pin no in brackets)
         self.lab = ttk.Label(self,width = 11,
                              text = 'GPIO ' + str(self.pin) + '(' + str(phys) + ')')
         self.lab.grid(row = 0,column = 0)
         
+        # create a label to show mode
+        self.mode_text = tk.StringVar()
+        self.mode_lab = ttk.Label(self,width = 7,textvariable = self.mode_text)
+        self.mode_lab.grid(row = 0,column = 1)
         # create a Light
         self.light = Light(self,height = 20,width = 30)
-        self.light.grid(row = 0,column = 1)
+        self.light.grid(row = 0,column = 2)
     
     # check to see if state has changed and switch light if needed             
     def check(self,mask):
         now = mask & self.mask
        
-        if(now != self.high):
+        if now != self.high:
             self.high = now
             if(now):
                 self.light.on()
             else:
                 self.light.off()
+                
+    def check_mode(self):
+        mode = pi.get_mode(self.pin)
+        if mode != self.mode:
+            self.mode = mode
+            self.mode_text.set(modes[mode])
     
 # a Mainframe object contains the widgets that make up the user interface              
 class Mainframe(tk.Frame):
@@ -131,7 +145,7 @@ class Mainframe(tk.Frame):
         # lay out the frames that show pin status
         i = 0
         row = 0
-        while(i < no):
+        while i < no:
             for col in range(COLS):
                 if i < no:
                     pf = PinFrame(self,pins[i][0],pins[i][1])
@@ -140,11 +154,21 @@ class Mainframe(tk.Frame):
                     i += 1
             row += 1
         
+        # Button to refresh modes
+        ttk.Button(self,text = 'Refresh modes',
+                   command = self.getmodes).grid(row = 10,column = 0)
+        
+        # Button to change host
+        ttk.Button(self,text = 'Change host',
+                   command = master.restart).grid(row = 10,column = 1)
         # I like to have a quit button
-        ttk.Button(self,text = 'Quit',command = master.onQuit).grid(row = 10)
+        ttk.Button(self,text = 'Quit',
+                   command = master.onQuit).grid(row = 10,column = 2)
+        
+        self.getmodes()
         self.getpins()
     
-    # called every REFRASH_RATE msec to update the display
+    # called every REFRESH_RATE msec to update the display
     def getpins(self):
         mask = pi.read_bank_1()
         
@@ -153,8 +177,16 @@ class Mainframe(tk.Frame):
             i += 1
             
         # set up the next call
-        self.after(REFRESH_RATE,self.getpins)
-    
+        self.timerid = self.after(REFRESH_RATE,self.getpins)
+        
+    def getmodes(self):
+        for i in range(len(self.pinframes)):
+            self.pinframes[i].check_mode()
+            i += 1
+
+    def cleartimer(self):
+        self.after_cancel(self.timerid)
+        
 # a single App object represents the program itself
 # it has a Mainframe which handles the UI
 class App(tk.Tk):
@@ -163,12 +195,13 @@ class App(tk.Tk):
         
         # trap close (X) button so we can cleanup
         self.protocol('WM_DELETE_WINDOW',self.onQuit)
+        self.window = None
         
         # do cosmetics to main window
-        self.title(TITLE)
+        self.title(TITLE + host)
         
         # check to see if we can talk to the pigpio daemon
-        if(not self.checkdaemon(host,port)):
+        if not self.checkdaemon(host,port):
             self.onQuit()
             return
         
@@ -176,18 +209,21 @@ class App(tk.Tk):
         global pi        
         pi = pg.pi(host,port)
         
-        Mainframe(self).grid()
+        self.window = Mainframe(self)
+        self.window.grid()
         
     def checkdaemon(self,host,port):
         # check to see if the target machine is on the network
         # and the demon seems to be running
-        # returns False if there is an error        # first see if host is on network
+        # returns False if there is an error
+        # first see if host is on network
         try:
             # n.b.on a raspberry pi this can take a long time to fail
             s.gethostbyname(host)
         except:
-            #mb.showerror('pigpio',"Cannot find host " + host)
+            # Hide the window
             self.withdraw()
+            # show Config dialog
             Config(self,"Cannot find host " + host)
             # abort program
             return False
@@ -204,10 +240,21 @@ class App(tk.Tk):
                 sock.close()
         return True
         
+    # restart to change host
+    def restart(self):
+        global restart
+        global host
+        host = ''
+        restart = True
+        self.onQuit()
+        
     # clean up and exit
     def onQuit(self):
+        if self.window != None:
+            self.window.cleartimer()
+            
         self.destroy()
-        if(pi != None):
+        if pi != None:
             pi.stop()
 
 restart = True
